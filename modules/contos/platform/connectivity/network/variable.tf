cd7291a4e4d8746a8897754b2daa73ca556561c3
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-08-21 17:54:06 +0530

    Update variable.tf

--- modules/contos/platform/connectivity/network/variable.tf
+++ modules/contos/platform/connectivity/network/variable.tf
@@ -1,1 +1,18 @@
-
+variable "vnet-name" {
+    type = string
+}
+variable "vnet_address_space" {
+    type = list(string)
+}
+variable "subnet-name" {
+    type = string
+}
+variable "subnet-address-prefix" {
+    type = list(string)
+}
+variable "resource-group-name" {
+    type = string
+}
+variable "location" {
+    type = string
+}
