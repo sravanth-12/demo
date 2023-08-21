cdab8040f387476b24b2e163a012db8cc7035aa4
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-08-21 17:53:03 +0530

    Update main.tf

--- modules/contos/platform/connectivity/network/main.tf
+++ modules/contos/platform/connectivity/network/main.tf
@@ -1,1 +1,14 @@
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet-name
+  address_space       = var.vnet_address_space
+  location            = var.location
+  resource_group_name = var.resource-group-name
+} 
+
+resource "azurerm_subnet" "vnet-subnet" {
+  name                 = var.subnet-name
+  resource_group_name  = var.resource-group-name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet-address-prefix
+}
