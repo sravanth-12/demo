6c12d57d4c540f3e1bc681c5a06c21e48698b593
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-08-22 14:51:09 +0530

    Update variable.tf

--- modules/contos/platform/identity/storage/variable.tf
+++ modules/contos/platform/identity/storage/variable.tf
@@ -1,1 +1,18 @@
-
+variable "primary_database1" {
+    type = string
+}
+variable "resource-group1-name" {
+    type = string
+}
+variable "location1" {
+    type = string
+}
+variable "primary_database1_version" {
+    type = string
+}
+variable "primary_database1_admin" {
+    type = string
+}
+variable "primary_database1_password" {
+    type = string
+}
