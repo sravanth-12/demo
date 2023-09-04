b0e1ccd86be964922566fb9fdb70491dae5f002a
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-09-04 10:19:12 +0530

    Create variable.tf

---
+++ modules/contos/platform/connectivity/key/variable.tf
@@ -0,0 +1,27 @@
+variable "app_vault-name" {
+  type = string
+}
+variable "resource-group-name" {
+  type = string
+}
+variable "location" {
+  type = string
+}
+variable "tenant_id" {
+  type = string
+}
+variable "soft_delete_retention_days" {
+  type = number
+}
+variable "purge_protection_enabled" {
+  type = bool
+}
+variable "sku_name" {
+  type = string
+}
+variable "vmpassword-name" {
+  type = string
+}
+variable "vmpassword-value" {
+  type = string
+}
