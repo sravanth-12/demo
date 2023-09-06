805a572d157b4a67556e3406033d154a6c87bac3
sravanth-12 <136778894+sravanth-12@users.noreply.github.com>
2023-09-06 21:32:47 +0530

    Update variable.tf

--- modules/variable.tf
+++ modules/variable.tf
@@ -395,3 +395,35 @@ variable "vmpassword-name" {
 variable "vmpassword-value" {
   type = string
 }
+
+variable "firewall_name" {
+  type = string
+}
+variable "firewall_sku_name" {
+  type = string
+}
+variable "firewall_sku_tier" {
+  type = string
+}
+/*variable "ip_configuration_name" {
+  type = string
+}*/
+variable "public_ip_firewall_name" {
+  type = string
+}
+variable "public_ip_firewall_allocation_method" {
+  type = string
+}
+variable "public_ip_firewall_sku" {
+  type = string
+}
+variable "firewallsubnet_name" {
+  type = string
+}
+variable "firewallsubnet_address_prefixes" {
+  type = list(string)
+}
+
+variable "vnet-name" {
+ type = string
+}
